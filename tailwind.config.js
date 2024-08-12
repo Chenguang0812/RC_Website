5f8682e19c7c93f6bfa62d629709092d3a943de9
Chenguang0812 <rcstudiochenguang@gmail.com>
2024-08-12 14:52:53 +0800

    Font3

--- tailwind.config.js
+++ tailwind.config.js
@@ -8,12 +8,6 @@ module.exports = {
         'web-green': '#4ECE80',
         bityo: '#17FFAD',
         primary: {
-          'white-100': '#FFFFFF',
-          'black-100': '#2C2C2C',
-          'black-200': '#3D3D3D',
-          'red-100': '#E25353',
-          'red-200': '#FF9797',
-          'black-500': '#',
         },
       },
     },
