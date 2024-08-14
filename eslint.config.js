3df107cff4a6aec7cbb16992fa45c592ee1f21a7
Chenguang0812 <rcstudiochenguang@gmail.com>
2024-08-14 15:08:24 +0800

    appplication-name

--- eslint.config.js
+++ eslint.config.js
@@ -1,13 +1,11 @@
-import pluginVue from 'eslint-plugin-vue'
+import globals from "globals";
+import pluginJs from "@eslint/js";
+import pluginVue from "eslint-plugin-vue";
+
+
 export default [
-  // add more generic rulesets here, such as:
-  // js.configs.recommended,
-  ...pluginVue.configs['flat/recommended'],
-  // ...pluginVue.configs['flat/vue2-recommended'], // Use this if you are using Vue.js 2.x.
-  {
-    rules: {
-      // override/add rules settings here, such as:
-      // 'vue/no-unused-vars': 'error'
-    },
-  },
-]
+  { files: ["**/*.{js,mjs,cjs,vue}"] },
+  { languageOptions: { globals: globals.browser } },
+  pluginJs.configs.recommended,
+  ...pluginVue.configs["flat/essential"],
+];
