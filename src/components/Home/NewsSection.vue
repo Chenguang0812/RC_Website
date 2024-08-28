ff323a5a1b1683bccc95378cd5f7b48f9ba9d47b
Chenguang0812 <rcstudiochenguang@gmail.com>
2024-08-28 18:04:31 +0800

    三振預告1

--- src/components/Home/NewsSection.vue
+++ src/components/Home/NewsSection.vue
@@ -13,8 +13,8 @@
           />
         </div>
       </div>
-      <div ref="newsContainer" class="h-[400px] overflow-y-auto custom-scrollbar">
-        <div ref="newsWrapper">
+      <div class="h-[400px] overflow-y-auto custom-scrollbar">
+        <div>
           <div
             v-for="item in newsItems"
             :key="item.id"
@@ -35,11 +35,13 @@
                   </p>
                 </div>
                 <div class="w-1/5 flex-shrink-0">
-                  <img
-                    :src="item.image"
-                    :alt="item.title"
-                    class="w-full h-full object-cover"
-                  />
+                  <div class="aspect-square w-full h-full relative">
+                    <img
+                      :src="item.image"
+                      :alt="item.title"
+                      class="absolute inset-0 w-full h-full object-cover object-center"
+                    />
+                  </div>
                 </div>
               </div>
             </a>
@@ -64,7 +66,7 @@ export default {
           link: "#",
         },
         {
-          id: 1,
+          id: 2,
           title: "最新》林德豐一部片剪了27天，跳抖肩舞懺悔。",
           image: "/道歉.webp",
           date: "2024年8月5日 週六 下午1:15",
@@ -72,21 +74,21 @@ export default {
             "https://drive.google.com/file/d/1hi5-lJv-x7WlovFzi7CSeaU6G1PQWe9K/view?usp=sharing",
         },
         {
-          id: 2,
+          id: 3,
           title: "最新》林德豐新影片上線，揭示剪輯背後的辛酸。",
           image: "/小鬼風.webp",
           date: "2024年8月3日 週六 下午2:00",
           link: "https://rcstudio.tw/No",
         },
         {
-          id: 3,
+          id: 4,
           title: "RC Studio擴大團隊，招募新人才。",
           image: "/阿烜.webp",
           date: "2024年8月2日 週五 上午10:15",
           link: "https://rcstudio.tw/joinUS",
         },
         {
-          id: 4,
+          id: 5,
           title: "RC Studio擴大營業項目：唱、跳、RAP、籃球",
           image: "/亞洲舞王蔡徐坤.webp",
           date: "2024年8月1日 週四 下午4:37",
@@ -99,15 +101,8 @@ export default {
 </script>
 
 <style scoped>
-.overflow-x-scroll {
-  overflow-x: scroll;
-  overflow-y: hidden;
-  white-space: nowrap;
-  -webkit-overflow-scrolling: touch;
-}
-
 .custom-scrollbar::-webkit-scrollbar {
-  height: 8px;
+  width: 8px;
 }
 
 .custom-scrollbar::-webkit-scrollbar-track {
@@ -127,8 +122,6 @@ export default {
 /* For Firefox */
 .custom-scrollbar {
   scrollbar-width: thin;
-  scrollbar-color: #f1f1f1;
+  scrollbar-color: #c2c2c2 #f1f1f1;
 }
-
-/* Dark mode scrollbar */
 </style>
